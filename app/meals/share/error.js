85c16fa6919d7ae66cb73ce124fff5379ff7515f
Saksham Jain <wgd1337.sakshamjain@gmail.com>
2025-06-19 21:02:11 +0530

    Initial Commit

---
+++ app/meals/share/error.js
@@ -0,0 +1,11 @@
+'use client'
+
+
+export default function Error(){
+    return(
+        <main className="error">
+            <h1>An error occured!</h1>
+            <p>Failed to share your meal.</p>
+        </main>
+    );
+}
